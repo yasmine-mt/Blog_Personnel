e9e3d445f173850e94ba67c58c522828fe3773ff
Yasmine Maataoui <yasminemaataoui12@gmail.com>
2024-06-01 16:48:52 +0100

    Creation of Project

---
+++ src/main/java/emsi/blog_personnel/BlogPersonnelApplication.java
@@ -0,0 +1,13 @@
+package emsi.blog_personnel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogPersonnelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogPersonnelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/emsi/blog_personnel/BlogPersonnelApplicationTests.java
@@ -0,0 +1,13 @@
+package emsi.blog_personnel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogPersonnelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
